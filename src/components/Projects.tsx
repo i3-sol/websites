8c0b557be65e109b5727b0f78238d8a8d0634241
ThunderBird0121 <spacecalian0121@gmail.com>
2022-12-15 21:45:05 +0100

    fix: projects layout

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,6 +2,7 @@ import { useI18n } from '@solid-primitives/i18n'
 import { For } from 'solid-js'
 import { projects } from '~/const/projects.conts'
 import { A } from 'solid-start'
+import { Project } from "~/models/projects.model";
 
 export default function Projects() {
 	const [t] = useI18n()
@@ -10,9 +11,9 @@ export default function Projects() {
 			<section class={'text-ebony-900 dark:text-gray-200 flex flex-col gap-4'}>
 				<h2 class={'font-semibold text-xl sm:text-2xl'}>{t('projects')}</h2>
 
-				<div class={'flex flex-col md:flex-row md:wrap gap-5'}>
+				<div class={'grid grid-flow-row auto-rows-fr items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'}>
 					<For each={projects}>
-						{(project) =>
+						{(project: Project) =>
 							<A href={project.url} target={'_blank'} class={'rounded-lg flex flex-col duration-300 hover:scale-105'}>
 								<img src={project.imageSrc} alt={`${project.name} front`} class={'rounded-t-lg'} />
 								<div class={'p-4 text-gray-600 dark:text-gray-200 bg-gray-300 dark:bg-slate-900 font-semibold rounded-b-lg'}>
@@ -22,7 +23,6 @@ export default function Projects() {
 						}
 					</For>
 				</div>
-
 			</section>
 		</>
 	)
