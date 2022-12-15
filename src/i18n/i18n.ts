3fa829c55def12be6f52224f2fc65b1e16da6385
ThunderBird0121 <spacecalian0121@gmail.com>
2022-12-15 20:46:35 +0100

    fix: adjust texts

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -2,11 +2,11 @@ export const availableLanguages = ['es', 'en']
 
 export const i18n = {
 	'es': {
-		'hello': 'Hola',
-		'about-me': 'Front-end Developer con más de 4 años de experiencia. Me encanta experimentar con diferentes tecnologías. Cada día se puede aprender algo.',
+		'hello': 'Hola, soy Clara',
+		'about-me': 'Front-end Developer con más de 4 años de experiencia. Me encanta experimentar con diferentes tecnologías. Cada día se puede aprender algo nuevo.',
 		'follow-me': 'Sígueme',
 		'tools': 'También he trabajado con estas herramientas',
 		'projects': 'Algunos proyectos personales',
-		'copyright': '© 2023 Clara Castillo. Developed with SolidJS.'
+		'copyright': '© 2023 Clara Castillo. Developed with SolidJS and TailwindCSS.'
 	},
 }
