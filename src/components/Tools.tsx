31d1c2df076e6c59107b9e53cf493f7b9d3852f4
ThunderBird0121 <spacecalian0121@gmail.com>
2022-12-19 20:30:26 +0100

    feat: update tool icons

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -1,9 +1,9 @@
 import { useI18n } from '@solid-primitives/i18n'
 import { For } from 'solid-js'
 import { tools } from '~/const/tools.const'
-import { darkMode } from '~/components/NavigatorBar';
-import { A } from "solid-start";
-import { Tool } from "~/models/tool.model";
+import { darkMode } from '~/components/NavigatorBar'
+import { A } from 'solid-start'
+import { Tool } from '~/models/tool.model'
 
 export default function Tools() {
 	const [t] = useI18n()
@@ -11,10 +11,10 @@ export default function Tools() {
 		<>
 			<section class={'flex flex-col gap-4 justify-center min-h-[70vh] text-ebony-900 dark:text-gray-200'}>
 				<h2 class={'font-semibold text-xl sm:text-2xl'}>{t('tools')}</h2>
-				<div class={'grid grid-flow-row auto-rows-fr items-center flex-shrink-0 w-full gap-3 text-center truncate select-none grid-cols-3 flex-shrink-0'}>
+				<div class={'grid grid-flow-row auto-rows-fr items-center flex-shrink-0 w-full gap-3 text-center truncate select-none grid-cols-3 flex-shrink-0 rounded-lg p-2'}>
 					<For each={tools}>
 						{(tool: Tool) =>
-							<A href={tool.url} target={'_blank'} class={'text-gray-200 rounded-lg flex flex-col gap-2 items-center justify-center align-middle p-4'} style={{'background': darkMode() ? tool.darkBackground : tool.lightBackground}}>
+							<A href={tool.url} target={'_blank'} class={'text-gray-200 rounded-lg flex flex-col gap-2 items-center justify-center align-middle p-4 duration-300 hover:scale-105'} style={{'background': darkMode() ? tool.darkBackground : tool.lightBackground}}>
 								{tool.icon}
 								<p class={'font-semibold text-sm md:text-base'}>{tool.name}</p>
 							</A>
--- src/const/tools.const.tsx
+++ src/const/tools.const.tsx
@@ -10,7 +10,7 @@ import MongoDBIcon from '~/components/icons/MongoDBIcon'
 import { Tool } from '~/models/tool.model'
 import RasaIcon from '~/components/icons/RasaIcon'
 import JenkinsIcon from '~/components/icons/JenkinsIcon'
-import PostmanIcon from '~/components/icons/PostmanIcon'
+import AstroIcon from '~/components/icons/AstroIcon'
 
 export const tools: Tool[] = [
 	// { name: 'JavaScript', icon: <JavascriptIcon class={'w-10 h-10'}/>, background: 'rgba(247, 223, 30, 0.5)' },
@@ -21,11 +21,12 @@ export const tools: Tool[] = [
 	{ name: 'SolidJS', icon: <SolidJSIcon class={'w-10 h-10'} />, lightBackground: '#2C4F7C', darkBackground: '#2C4F7C', url: 'https://www.solidjs.com/' },
 	{ name: 'VueJS', icon: <VueJSIcon class={'w-10 h-10'} />, lightBackground: '#269c5d', darkBackground: '#269c5d', url: 'https://vuejs.org/' },
 	{ name: 'Python', icon: <PythonIcon class={'w-10 h-10'} />, lightBackground: '#3776AB', darkBackground: '#3776AB', url: 'https://www.python.org/' },
+	{ name: 'Astro', icon: <AstroIcon class={'w-10 h-10'} />, lightBackground: '#FF5D01', darkBackground: '#FF3000', url: 'https://astro.build/' },
 	{ name: 'NodeJS', icon: <NodeJSIcon class={'w-10 h-10'} />, lightBackground: '#339933', darkBackground: '#146b14', url: 'https://nodejs.org/' },
-	{ name: 'Socket.io', icon: <SocketIOIcon class={'w-10 h-10'} />, lightBackground: '#626262', darkBackground: '#343434', url: 'https://socket.io/' },
 	{ name: 'Jira', icon: <JiraIcon class={'w-10 h-10'} />, lightBackground: '#0052CC', darkBackground: '#0028ad', url: 'https://www.atlassian.com/es/software/jira' },
 	{ name: 'MongoDB', icon: <MongoDBIcon class={'w-10 h-10'} />, lightBackground: '#47A248', darkBackground: '#207521', url: 'https://www.mongodb.com/' },
 	{ name: 'Rasa', icon: <RasaIcon class={'w-10 h-10'} />, lightBackground: '#5A17EE', darkBackground: '#5A17EE', url: 'https://rasa.com/' },
 	{ name: 'Jenkins', icon: <JenkinsIcon class={'w-10 h-10'} />, lightBackground: '#D24939', darkBackground: '#B62117', url: 'https://www.jenkins.io/' },
-	{ name: 'Postman', icon: <PostmanIcon class={'w-10 h-10'} />, lightBackground: '#FF6C37', darkBackground: '#ed2f0f', url: 'https://www.postman.com/' },
+	{ name: 'Socket.io', icon: <SocketIOIcon class={'w-10 h-10'} />, lightBackground: '#626262', darkBackground: '#343434', url: 'https://socket.io/' },
+	// { name: 'Postman', icon: <PostmanIcon class={'w-10 h-10'} />, lightBackground: '#FF6C37', darkBackground: '#ed2f0f', url: 'https://www.postman.com/' },
 ]
